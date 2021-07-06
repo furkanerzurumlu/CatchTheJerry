aafc01dae6ac365995a7507c2e0690a421a68f8f
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2021-07-06 16:10:30 +0300

    first commint

---
+++ CatchTheJerryGame/ViewController.swift
@@ -0,0 +1,160 @@
+//
+//  ViewController.swift
+//  CatchTheJerryGame
+//
+//  Created by Furkan Erzurumlu on 2.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Variables
+    
+    var score = 0
+    var timer = Timer()
+    var counter = 0
+    var jerryArray = [UIImageView]()
+    var hideTimer = Timer()
+    var highScore = 0
+    
+    //Views
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
+    @IBOutlet weak var jerry1: UIImageView!
+    @IBOutlet weak var jerry2: UIImageView!
+    @IBOutlet weak var jerry3: UIImageView!
+    
+    @IBOutlet weak var jerry4: UIImageView!
+    @IBOutlet weak var jerry5: UIImageView!
+    @IBOutlet weak var jerry6: UIImageView!
+    
+    @IBOutlet weak var jerry7: UIImageView!
+    @IBOutlet weak var jerry8: UIImageView!
+    @IBOutlet weak var jerry9: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        let storedHighScore = UserDefaults.standard.object(forKey: "highScore")
+        
+        if storedHighScore == nil {
+            highScore = 0
+            highScoreLabel.text = "Highscore: \(highScore)"
+        }
+        if let newScore = storedHighScore as? Int {
+            highScore = newScore
+            highScoreLabel.text = "Highscore: \(highScore)"
+        }
+        
+        //Images
+        
+        jerry1.isUserInteractionEnabled = true
+        jerry2.isUserInteractionEnabled = true
+        jerry3.isUserInteractionEnabled = true
+        jerry4.isUserInteractionEnabled = true
+        jerry5.isUserInteractionEnabled = true
+        jerry6.isUserInteractionEnabled = true
+        jerry7.isUserInteractionEnabled = true
+        jerry8.isUserInteractionEnabled = true
+        jerry9.isUserInteractionEnabled = true
+        
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        
+        
+        jerry1.addGestureRecognizer(recognizer1)
+        jerry2.addGestureRecognizer(recognizer2)
+        jerry3.addGestureRecognizer(recognizer3)
+        jerry4.addGestureRecognizer(recognizer4)
+        jerry5.addGestureRecognizer(recognizer5)
+        jerry6.addGestureRecognizer(recognizer6)
+        jerry7.addGestureRecognizer(recognizer7)
+        jerry8.addGestureRecognizer(recognizer8)
+        jerry9.addGestureRecognizer(recognizer9)
+        
+        jerryArray = [jerry1,jerry2,jerry3,jerry4,jerry5,jerry6,jerry7,jerry8,jerry9]
+
+        //Timers
+        
+        counter = 10
+        timeLabel.text = "\(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideJerry), userInfo: nil, repeats: true)
+        
+        hideJerry()
+        
+    }
+    @objc func hideJerry(){
+        for jerry in jerryArray {
+            jerry.isHidden = true
+        }
+        let random = Int (arc4random_uniform(UInt32(jerryArray.count - 1)))
+        jerryArray[random].isHidden = false
+        
+        
+    }
+    
+    @objc func increaseScore(){
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+    }
+    @objc func countDown(){
+        counter -= 1
+        timeLabel.text = "\(counter)"
+        
+        if counter == 0 {
+            timer.invalidate()
+            hideTimer.invalidate()
+            
+            for jerry in jerryArray {
+                jerry.isHidden = true
+            }
+            
+            
+            if self.score > self.highScore {
+                self.highScore  = self.score
+                highScoreLabel.text = "Highscore: \(self.highScore)"
+                UserDefaults.standard.set(self.highScore, forKey: "highScore")
+            }
+            
+            
+            
+            let alert = UIAlertController(title: "Times's Up!", message: "Do You Want to Play Again !", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            
+            let replayButton = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default) { (UIAlertAction) in
+                
+                self.score = 0
+                self.scoreLabel.text = "Scoer: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text = "\(self.counter)"
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideJerry), userInfo: nil, repeats: true)
+                
+            }
+            
+            alert.addAction(okButton)
+            alert.addAction(replayButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+
+}
+
